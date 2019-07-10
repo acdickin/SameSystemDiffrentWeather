6ff07264a3ee75ceb56cfc2bbaa0ab2a6d5a0e50
Andrew Cosmic (INFOSYS LIMITED) <v-andick@microsoft.com>
2019-07-10 12:10:36 -0700

    Add project files.

---
+++ SameSystemDiffrentWeather/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SameSystemDiffrentWeather.Startup))]
+namespace SameSystemDiffrentWeather
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
